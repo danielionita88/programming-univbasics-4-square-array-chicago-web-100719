634ae26fbc9d165a321051be938fc6faa432fa58
Daniel I <danielionita88@yahoo.com>
2019-09-16 19:36:10 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   squared_number = []
   i = 0 
 while i < array.length do
-  squared_number.push(array[i] * array[i])
+  squared_number.push(array[i] ** 2)
  i += 1
 end
 squared_number
